45fc48a0d77cac90aafcd1e44faa516edf1c245b
inha921119 <dlsgk921119@gmail.com>
2023-04-20 14:20:35 +0900

    getArticles, doModify, doDelete에 generic 적용

---
+++ src/main/java/com/koreaIT/demo/service/ArticleService.java
@@ -0,0 +1,45 @@
+package com.koreaIT.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.koreaIT.demo.repository.ArticleRepository;
+import com.koreaIT.demo.vo.Article;
+
+@Service
+public class ArticleService {
+	
+	private ArticleRepository articleRepository;
+	
+	@Autowired
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
+	
+	public void writeArticle(String title, String body) {
+		articleRepository.writeArticle(title, body);
+	}
+	
+	public int getLastInsertId() {
+		return articleRepository.getLastInsertId();
+	}
+	
+	public Article getArticleById(int id) {
+		return articleRepository.getArticleById(id);
+	}
+	
+	public List<Article> getArticles(){
+		return articleRepository.getArticles();
+	}
+	
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+	
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+	
+}
